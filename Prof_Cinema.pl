58f0f8421a9838bb98f7fa484e35e166dde96307
laraalqahtani <116515572+laraalqahtani@users.noreply.github.com>
2024-02-27 17:24:48 +0300

    Add files via upload

---
+++ Prof_Cinema.pl
@@ -0,0 +1,419 @@
+%------Action-----
+%Hollywood
+movie('DJANGO UNCHAINED', 'Action', 'Hollywood', 'R', 'Winter').
+movie('KUNG FU HUSTLE', 'Action', 'Hollywood', 'R', 'Winter').
+movie('TENET', 'Action', 'Hollywood', 'PG-13', 'Summer').
+movie('WONDER WOMAN 1984', 'Action', 'Hollywood', 'PG-13', 'Winter').
+movie('TERMINATOR 2: JUDGMENT DAY', 'Action', 'Hollywood', 'R', 'Summer').
+movie('GLADIATOR', 'Action', 'Hollywood', 'R', 'Autumn').
+movie('RAIDERS OF THE LOST ARK', 'Action', 'Hollywood', 'PG', 'Spring').
+movie('DIE HARD', 'Action', 'Hollywood', 'R', 'Summer').
+movie('THE MATRIX', 'Action', 'Hollywood', 'R', 'Winter').
+movie('THE TERMINATOR', 'Action', 'Hollywood', 'R', 'Winter').
+movie('INCEPTION', 'Action', 'Hollywood', 'PG-13', 'Spring').
+movie('AVATAR', 'Action', 'Hollywood', 'PG-13', 'Summer').
+movie('PANDA', 'Action', 'Hollywood', 'G', 'Spring').
+
+%Hallyuwood
+movie('Wanted', 'Action', 'Hallyuwood', 'PG-13', 'Winter').
+movie('Train to Busan', 'Action', 'Hallyuwood', 'PG', 'Summer').
+movie('TRAIN TO BUSAN', 'Action', 'Hallyuwood', 'R', 'Summer').
+movie('THE MAN FROM NOWHERE (AJUSSI)', 'Action', 'Hallyuwood', 'G', 'Winter').
+
+%Bollywood
+movie('Mysterious Nights', 'Action', 'Bollywood', 'PG', 'Winter').
+movie('BAAGHI 3', 'Action', 'Bollywood', 'PG-13', 'Spring').
+movie('KICK', 'Action', 'Bollywood', 'G', 'Summer').
+movie('WAR', 'Action', 'Bollywood', 'R', 'Autumn').
+movie('SADAK 2', 'Action', 'Bollywood', 'PG-13', 'Winter').
+movie('CHENNAI EXPRESS', 'Action', 'Bollywood', 'PG-13', 'Spring').
+
+%Hogawood
+movie('RURONI KENSHIN', 'Action', 'Hogawood', 'PG-13', 'Spring').
+movie('SHIN GODZILLA', 'Action', 'Hogawood', 'PG', 'Autumn').
+movie('Oldboy', 'Action', 'Hogawood', 'R', 'Summer').
+movie('The Villainess', 'Action', 'Hogawood', 'G', 'Winter').
+
+%No Preference
+movie('THE DARK KNIGHT', 'Action', 'No Preference', 'PG-13', 'Summer').
+movie('DIE HARD', 'Action', 'No Preference', 'R', 'Summer').
+movie('RAIDERS OF THE LOST ARK', 'Action', 'No Preference', 'PG', 'Spring').
+movie('Mad Max: Fury Road', 'Action', 'No Preference', 'G', 'Spring').
+
+
+
+%--------Romance--------
+%Hollywood
+movie('NOTTING HILL', 'Romance', 'Hollywood', 'PG-13', 'Spring').
+movie('BEFORE SUNSET', 'Romance', 'Hollywood', 'R', 'Summer').
+movie('PALM SPRINGS', 'Romance', 'Hollywood', 'R', 'Summer').
+movie('MALCOLM & MARIE', 'Romance', 'Hollywood', 'R', 'Winter').
+movie('ETERNAL SUNSHINE OF THE SPOTLESS MIND', 'Romance', 'Hollywood', 'R', 'Spring').
+movie('TITANIC', 'Romance', 'Hollywood', 'PG-13', 'Winter').
+movie('THE NOTEBOOK', 'Romance', 'Hollywood', 'PG-13', 'Autumn').
+movie('CASABLANCA', 'Romance', 'Hollywood', 'PG', 'Summer').
+movie('500 DAYS OF SUMMER', 'Romance', 'Hollywood', 'PG-13', 'Summer').
+movie('PANDA2', 'Romance', 'Hollywood', 'G', 'Spring').
+movie('TITANIC', 'Romance', 'Hollywood', 'PG-13', 'Winter').
+movie('GONE WITH THE WIND', 'Romance', 'Hollywood', 'G', 'Autumn').
+movie('HER', 'Romance', 'Hollywood', 'R', 'Winter').
+
+
+%Hallyuwood
+movie('MY SASSY GIRL', 'Romance', 'Hallyuwood', 'PG-13', 'Spring').
+movie('TRAIN TO BUSAN"', 'Romance', 'Hallyuwood', 'PG', 'Summer').
+movie('LOVELY TOTO', 'Romance', 'Hallyuwood', 'PG', 'Summer').
+movie('Before Sunrise', 'Romance', 'Hallyuwood', 'G', 'Summer').
+movie('My Love from the Star ', 'Romance', 'Hallyuwood', 'R', 'Summer').
+
+
+%Bollywood
+movie('LOVE AAJ KAL', 'Romance', 'Bollywood', 'PG-13', 'Spring').
+movie('DIL BECHARA', 'Romance', 'Bollywood', 'PG', 'Summer').
+movie('DDLJ', 'Romance', 'Bollywood', 'G', 'Summer').
+movie('DILWALE', 'Romance', 'Bollywood', 'PG-13', 'Spring').
+movie('LA LA LAND2', 'Romance', 'Bollywood', 'R', 'Winter').
+
+%Hogawood
+movie('Oldboy', 'Romance', 'Hogawood', 'PG', 'Autumn').
+movie('Your Name (Kimi No Na Wa)', 'Romance', 'Hogawood', 'G', 'Autumn').
+movie('The Handmaiden', 'Romance', 'Hogawood', 'R', 'Summer').
+movie('Shin Godzilla', 'Romance', 'Hogawood', 'PG-13', 'Winter').
+
+
+
+%No Preference
+
+movie('KABHI KHUSHI KABHIE GHAM', 'Romance', 'No Preference', 'PG', 'Summer').
+movie('ETERNAL SUNSHINE OF THE SPOTLESS MIND', 'Romance', 'No Preference', 'R', 'Spring').
+movie('Sunset', 'Romance', 'No Preference', 'G', 'Winter').
+movie('LA LA LAND', 'Romance', 'No Preference', 'PG-13', 'Winter').
+movie('ETERNAL SUNSHINE OF THE SPOTLESS MIND', 'Romance', 'No Preference', 'R', 'Spring').
+
+
+%-------Comedy------
+%Hollywood
+
+movie('ANCHORMAN: THE LEGEND OF RON BURGUNDY', 'Comedy', 'Hollywood', 'R', 'Fall').
+movie('THE FRENCH DISPATCH', 'Comedy', 'Hollywood', 'R', 'Autumn').
+movie('SUPERBAD', 'Comedy', 'Hollywood', 'R', 'Summer').
+movie('THE HANGOVER', 'Comedy', 'Hollywood', 'R', 'Autumn').
+movie('PANDA DOD', 'Comedy', 'Hollywood', 'G', 'Spring').
+movie('Home Alone', 'Comedy', 'Hollywood', 'PG', 'Winter').
+movie('Dump an Dumper', 'Comedy', 'Hollywood', 'PG-13', 'Summer').
+
+%Hallyuwood
+movie('SUNNY', 'Comedy', 'Hallyuwood', 'PG-13', 'Summer').
+movie(' The Farewell', 'Comedy', 'Hallyuwood', 'PG-13', 'Spring').
+movie('EXTREME JOB', 'Comedy', 'Hallyuwood', 'PG-13', 'Winter').
+movie(' Klaus', 'Comedy', 'Hallyuwood', 'PG', 'Winter').
+movie('Midnight Runners', 'Comedy', 'Hallyuwood', 'R', 'Summer').
+movie('Miss Granny', 'Comedy', 'Hallyuwood', 'G', 'Autumn').
+movie('Boomerang Family', 'Comedy', 'Hallyuwood', 'G', 'Winter').
+
+
+%Bollywood
+movie('Blue', 'Comedy', 'Bollywood', 'PG', 'Spring').
+movie('COOLIE NO. 1', 'Comedy', 'Bollywood', 'PG', 'Winter').
+movie('GULABO SITABO', 'Comedy', 'Bollywood', 'PG-13', 'Summer').
+movie('KABHI KHUSHI KABHIE GHAM', 'Comedy', 'Bollywood', 'PG', 'Autumn').
+movie('Dhamaal', 'Comedy', 'Bollywood', 'G', 'Summer').
+movie('Masti', 'Comedy', 'Bollywood', 'R', 'Summer').
+movie('Mumbai Matinee', 'Comedy', 'Bollywood', 'R', 'Spring').
+
+%Hogawood
+movie('Didi', 'Comedy', 'Hogawood', 'PG', 'Summer').
+movie('SWING GIRLS', 'Comedy', 'Hogawood', 'PG-13', 'Spring').
+movie('SHALL WE DANCE?', 'Comedy', 'Hogawood', 'PG', 'Autumn').
+movie('The Apology King', 'Comedy', 'Hogawood', 'R', 'Summer').
+movie('Cyborg She', 'Comedy', 'Hogawood', 'R', 'Spring').
+movie('Not Quite Dead Yet', 'Comedy', 'Hogawood', 'G', 'Summer').
+movie('The Fable', 'Comedy', 'Hogawood', 'PG-13', 'Winter').
+movie('Dance With Me', 'Comedy', 'Hogawood', 'PG', 'Autumn').
+
+%No Preference
+movie('Day in LA', 'Comedy', 'No Preference', 'G', 'Summer').
+movie('FERRIS BUELLER\'S DAY OFF', 'Comedy', 'No Preference', 'PG-13', 'Autumn').
+movie('THE GRAND BUDAPEST HOTEL', 'Comedy', 'No Preference', 'R', 'Spring').
+movie('Odd Squad', 'Comedy', 'No Preference', 'G', 'Winter').
+movie('Bad Boys', 'Comedy', 'No Preference', 'PG', 'Summer').
+movie('3 Idiots', 'Comedy', 'No Preference', 'PG-13', 'Spring').
+movie(' Get Him to the Greek', 'Comedy', 'No Preference', 'R', 'Summer').
+movie('Hello Ghosr', 'Comedy', 'No Preference', 'PG', 'Autumn').
+
+
+
+%---------Animation-------
+%Hollywood
+movie('SOUL', 'Animation', 'Hollywood', 'PG', 'Winter').
+movie('Raya and the Last Dragon', 'Animation', 'Hollywood', 'PG', 'Spring').
+movie('HOW TO TRAIN YOUR DRAGON', 'Animation', 'Hollywood', 'PG', 'Spring').
+movie('SHREK', 'Animation', 'Hollywood', 'PG', 'Summer').
+movie('FROZEN', 'Animation', 'Hollywood', 'PG-13', 'Winter').
+movie('Batman: Gotham by Gaslight', 'Animation', 'Hollywood', 'R', 'Winter').
+movie('COCO', 'Animation', 'Hollywood', 'PG', 'Fall').
+movie('MOANA', 'Animation', 'Hollywood', 'PG', 'Summer').
+movie('KUNG FU PANDA', 'Animation', 'Hollywood', 'PG', 'Summer').
+movie('PANDA3', 'Animation', 'Hollywood', 'G', 'Spring').
+movie('SPIRIT: STALLION OF THE CIMARRON', 'Animation', 'Hollywood', 'G', 'Spring').
+movie('THE LION KING', 'Animation', 'Hollywood', 'G', 'Spring').
+movie('TOY STORY 3', 'Animation', 'Hollywood', 'G', 'Autumn').
+movie('UP', 'Animation', 'Hollywood', 'PG', 'Summer').
+movie('TOY STORY 3', 'Animation', 'Hollywood', 'G', 'Winter').
+movie('RATATOUILLE', 'Animation', 'Hollywood', 'G', 'Summer').
+movie('WALL-E', 'Animation', 'Hollywood', 'G', 'Spring').
+movie('THE LION KING', 'Animation', 'Hollywood', 'G', 'Spring').
+
+
+%Hallyuwood
+movie('Cinnamon Girl', 'Animation', 'Hallyuwood', 'PG', 'Autumn').
+movie('YOUR NAME (KIMI NO NA WA)', 'Animation', 'Hallyuwood', 'PG', 'Winter').
+movie('Fiddler on the Roof', 'Animation', 'Hallyuwood', 'G', 'Winter').
+movie('Oseam',  'Animation', 'Hallyuwood', 'G', 'Autumn' ).
+movie('Wonderful Days', 'Animation', 'Hallyuwood', 'PG-13', 'Autumn' ).
+movie(' Heavy Metal ', 'Animation', 'Hallyuwood', 'R', 'Summer' ).
+%Bollywood
+movie('PUTO', 'Animation', 'Bollywood', 'PG', 'Autumn').
+movie('Kochadiyaan', 'Animation', 'Bollywood', 'R', 'Spring').
+movie('Hanuman', 'Animation', 'Bollywood', 'PG-13', 'Summer').
+movie('Roadside Romeo', 'Animation', 'Bollywood', 'PG-13', 'Winter').
+movie('Arjun : The warrior Prince', 'Animation', 'Bollywood', 'G', 'Summer').
+movie('Ramayana', 'Animation', 'Bollywood', 'R', 'Spring').
+
+%Hogawood
+movie('SPIRITED AWAY', 'Animation', 'Hogawood', 'PG', 'Autumn').
+movie(' Wolf Children', 'Animation', 'Hogawood', 'G', 'Winter').
+movie(' Mary and the Witchs Flower ', 'Animation', 'Hogawood', 'PG', 'Spring').
+movie(' A Silent Voice', 'Animation', 'Hogawood', 'PG', 'Winter').
+movie( 'Children Who Chase Lost Voices', 'Animation', 'Hogawood', 'PG-13', 'Winter').
+movie('Perfect Blue', 'Animation', 'Hogawood', 'R', 'Autumn').
+movie('Ghost In The Shell', 'Animation', 'Hogawood', 'R', 'Summer').
+movie('Princess Mononoke', 'Animation', 'Hogawood', 'PG-13', 'Winter').
+
+
+
+%No Preference
+movie('AOT', 'Animation', 'No Preference', 'R', 'Winter').
+movie('SPIDER-MAN: INTO THE SPIDER-VERSE', 'Animation', 'No Preference', 'PG', 'Winter').
+movie('Detective Conan: Zero the Enforcer', 'Animation', 'No Preference', 'PG-13', 'Autumn').
+movie('Ponyo', 'Animation', 'No Preference', 'G', 'Spring').
+movie('Akira', 'Animation', 'No Preference', 'R', 'Summer').
+movie('Inside Out', 'Animation', 'No Preference', 'PG-13', 'Spring').
+movie('Beowulf', 'Animation', 'No Preference', 'PG-13', 'Winter').
+movie('Alaadin', 'Animation', 'No Preference', 'PG', 'Summer').
+
+%------Crime-------
+%Hollywood
+movie('NO TIME TO DIE', 'Crime', 'Hollywood', 'PG-13', 'Autumn').
+movie('MEMENTO', 'Crime', 'Hollywood', 'R', 'Winter').
+movie('SE7EN', 'Crime', 'Hollywood', 'R', 'Autumn').
+movie('THE LITTLE THINGS', 'Crime', 'Hollywood', 'R', 'Winter').
+movie('PULP FICTION', 'Crime', 'Hollywood', 'R', 'Summer').
+movie('Crime 778', 'Crime', 'Hollywood', 'G', 'Spring').
+movie('UP', 'Crime', 'Hollywood', 'PG', 'Autumn').
+
+
+%Hallyuwood
+movie('THE CALL', 'Crime', 'Hogawood', 'PG-13', 'Spring').
+
+%Bollywood
+movie('Killer Khan', 'Crime', 'Bollywood', 'PG-13', 'Autumn').
+movie('MALANG', 'Crime', 'Bollywood', 'R', 'Winter').
+movie('Criminal 8', 'Crime', 'Bollywood', 'PG-13', 'Winter').
+
+%Hogawood
+movie('UNDERWORLD CHRONICLES', 'Crime', 'Hogawood', 'R', 'Autumn').
+movie('THE CALL', 'Crime', 'Hogawood', 'PG-13', 'Spring').
+
+
+%No Preference
+movie('THE GODFATHER', 'Crime', 'No Preference', 'R', 'Autumn').
+movie('HEAT', 'Crime', 'No Preference', 'R', 'Winter').
+movie('UGO', 'Crime', 'No Preference', 'R', 'Winter').
+movie('The Lady', 'Crime', 'No Preference', 'G', 'Summer').
+movie('Late Night Visitor ', 'Crime', 'No preference', 'PG', 'Spring').
+movie('Revenge ', 'Crime', 'No preference', 'PG-13', 'Spring').
+
+
+%---------Drama---------
+%Hollywood
+movie('THE SHAWSHANK REDEMPTION', 'Drama', 'Hollywood', 'R', 'Spring').
+movie('FORREST GUMP', 'Drama', 'Hollywood', 'PG-13', 'Autumn').
+movie('Let it snow', 'Drama', 'Hollywood', 'G', 'Winter').
+movie('NOMADLAND', 'Drama', 'Hollywood', 'R', 'Spring').
+movie('JUDAS AND THE BLACK MESSIAH', 'Drama', 'Hollywood', 'R', 'Winter').
+movie('A STAR IS BORN', 'Drama', 'Hollywood', 'R', 'Autumn').
+movie('THE GREEN MILE', 'Drama', 'Hollywood', 'R', 'Winter').
+movie('HER', 'Drama', 'Hollywood', 'R', 'Winter').
+movie('SCHINDLER\'S LIST', 'Drama', 'Hollywood', 'R', 'Autumn').
+movie('THE SOCIAL NETWORK', 'Drama', 'Hollywood', 'PG-13', 'Autumn').
+movie('JOKER', 'Drama', 'Hollywood', 'R', 'Fall').
+movie('koko', 'Drama', 'Hollywood', 'G', 'Spring').
+movie('HER', 'Drama', 'Hollywood', 'R', 'Winter').
+movie('ETERNAL SUNSHINE OF THE SPOTLESS MIND', 'Drama', 'Hollywood', 'R', 'Spring').
+movie('HER', 'Drama', 'Hollywood', 'R', 'Spring').
+movie('ETERNAL SUNSHINE OF THE SPOTLESS MIND', 'Drama', 'Hollywood', 'R', 'Summer').
+movie('THE SOCIAL NETWORK', 'Drama', 'Hollywood', 'PG-13', 'Autumn').
+movie('Anne ', 'Drama', 'Hollywood', 'PG', 'Spring').
+
+%Hallyuwood
+movie('OLD BOY', 'Drama', 'Hallyuwood', 'R', 'Winter').
+movie('BURNING', 'Drama', 'Hallyuwood', 'R', 'Autumn').
+movie('PARASITE', 'Drama', 'Hallyuwood', 'R', 'Autumn').
+movie('The breakfast club ', 'Drama', 'Hallyuwood', 'G', 'Spring').
+movie('The good nurses  ', 'Drama', 'Hallyuwood', 'PG', 'Spring').
+movie('erin brockovich ', 'Drama', 'Hallyuwood', 'PG-13', 'Spring').
+
+
+%Bollywood
+movie('THAPPAD', 'Drama', 'Bollywood', 'PG-13', 'Spring').
+movie('LUDO', 'Drama', 'Bollywood', 'PG-13', 'Autumn').
+movie('KABHI KHUSHI KABHIE GHAM', 'Drama', 'Bollywood', 'PG', 'Summer').
+movie('DANGAL', 'Drama', 'Bollywood', 'PG', 'Spring').
+movie('OLA ', 'Drama', 'Bollywood', 'G', 'Spring').
+movie('Clue', 'Drama', 'Bollywood', 'R', 'Spring').
+
+
+%Hogawood
+movie('Life', 'Drama', 'Hogawood', 'PG', 'Summer').
+movie('LIKE FATHER, LIKE SON (SOSHITE CHICHI NI NARU)', 'Drama', 'Hogawood', 'PG-13', 'Spring').
+movie('THE WIND RISES (KAZE TACHINU)', 'Drama', 'Hogawood', 'PG-13', 'Autumn').
+movie('war3', 'Drama', 'Hogawood', 'G', 'Spring').
+movie('Below Zero', 'Drama', 'Hogawood', 'R', 'Spring').
+
+
+%No Preference
+movie('Frozen Shadows', 'Drama', 'No Preference', 'R', 'Winter').
+movie('Tedy 4 ', 'Drama', 'No preference', 'G', 'Spring').
+movie('SNOW', 'Drama', 'No preference', 'PG', 'Spring').
+movie('CAMERON ', 'Drama', 'No preference', 'PG-13', 'Spring').
+
+
+% Recommendation predicate
+recommend_movie(Movie, Category, Language, Rating) :-
+    movie(Movie, Category, Language, Rating, _).
+
+% Options for preferences
+category_list(['Action', 'Romance', 'Animation', 'Comedy', 'Crime', 'Drama']).
+language_list(['Hollywood', 'Hallyuwood', 'Bollywood', 'Hogawood', 'No Preference']).
+rating_list(['G', 'PG', 'PG-13', 'R']).
+season_list(['Spring', 'Summer', 'Autumn', 'Winter']).
+
+% Get user preferences with options
+get_user_preferences(Category, Language, Rating) :-
+    category_list(Categories),
+    rating_list(Ratings),
+    language_list(Languages),
+    season_list(Seasons),
+
+  write('Choose your favorite movie category:'), nl,
+    display_options(Categories, 1),
+    read(CategoryIndex),
+    nth1(CategoryIndex, Categories, Category),
+
+    write('Choose your preferred movie language:'), nl,
+    display_options(Languages, 1),
+    read(LanguageIndex),
+    nth1(LanguageIndex, Languages, Language),
+
+    write('Choose your preferred movie rating:'), nl,
+    display_options(Ratings, 1),
+    read(RatingIndex),
+    nth1(RatingIndex, Ratings, Rating),
+
+    write('Choose your preferred season:'), nl,
+    display_options(Seasons, 1),
+    read(SeasonIndex),
+    nth1(SeasonIndex, Seasons, Season),
+    
+    
+    member(Language, Languages),
+    member(Season, Seasons).
+
+% Display options for the user to choose
+display_options([], _).
+display_options([Option | Rest], Index) :-
+    format('~w. ~w~n', [Index, Option]),
+    NextIndex is Index + 1,
+    display_options(Rest, NextIndex).
+
+% Find the closest movie based on category
+find_closest_movie(Category, LastRecommendedMovie) :-
+    movie(Movie, Category, _, _, _),
+    Movie \= LastRecommendedMovie.
+
+% Check if a movie with the exact category exists; otherwise, find the closest
+find_movie_or_closest(Category, LastRecommendedMovie, Movie) :-
+    (movie(Movie, Category, _, _, _),
+     Movie \= LastRecommendedMovie ->
+        true
+    ;
+        find_closest_movie(Category, LastRecommendedMovie)
+    ).
+
+% Print the closest movie based on user category or the closest available
+print_closest_movie_or_closest(Category, LastRecommendedMovie) :-
+    find_movie_or_closest(Category, LastRecommendedMovie, ClosestMovie),
+    movie(ClosestMovie, _, _, Rating, _),
+    write('Another close Recommendation in same category: '), write(ClosestMovie), nl,
+    write(' (Category: '), write(Category), write(', Rating: '), write(Rating), write(')'), nl,
+    ask_user_feedback(Feedback),
+    process_user_feedback(Feedback).
+
+% Interactive recommendation
+recommend_movie_for_user_with_closest :-
+    get_user_preferences(Category, Language, Rating),
+    find_closest_movie(Category, ''),
+    recommend_movie(Movie, Category, Language, Rating),
+    write('Recommended movie: '), write(Movie), nl,
+    write(' (Category: '), write(Category), write(', Rating: '), write(Rating), write(')'), nl,
+    print_closest_movie_or_closest(Category, Movie).
+
+
+% Ask the user if they like the recommendation
+ask_user_feedback(Feedback) :-
+    write('Do you like the recommendation?'), nl,
+    write('1. Yes'), nl,
+    write('2. No'), nl,
+    read(Feedback),
+
+    % Validate feedback input(Error Handling)
+    (Feedback = 1 ; Feedback = 2) ->
+        true
+    ;
+        write('Invalid input. Please enter 1 for Yes or 2 for No.'), nl,
+        ask_user_feedback(Feedback).
+
+
+% Thank you message and feedback form link
+thank_you_message :-
+    write('Thank you for using PROF Movie Recommendation System!'), nl,
+    write('We appreciate your feedback. Please provide your feedback through this form:'), nl,
+    write('https://docs.google.com/forms/d/e/1FAIpQLSeLwkrfjk3ru7BLP48QWj0FfQ1ae5VixG9bUqA3B-44uGipoQ/viewform?usp=sf_link'), nl.
+
+% Interactive recommendation with user feedback
+recommend_movie_for_user_after_feedback :-
+    get_user_preferences(Category, Language, Rating),
+    find_closest_movie(Category, ''),
+    recommend_movie(Movie, Category, Language, Rating),
+    write('Recommended movie: '), write(Movie), nl,
+    write(' (Category: '), write(Category),
+    write(', Rating: '), write(Rating), 
+    write(', Language: '), write(language), write(')'), nl,
+    ask_user_feedback(Feedback),
+    process_user_feedback(Feedback).
+
+% Process user feedback
+process_user_feedback(1) :-
+    thank_you_message, !. 
+
+process_user_feedback(2) :-
+    write('You didn\'t like the recommendation. Let\'s try another one.'), nl,
+   recommend_movie_for_user_after_feedback.
+
+% Start the recommendation system
+start :-
+    write('Welcome to the Movie Recommender PROF!'), nl,
+    write('---------------------------------'), nl,
+    write('Let\'s find the perfect movie for you.'), nl,
+    recommend_movie_for_user_with_closest.
